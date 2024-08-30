d093d91eff4f05263a1663fbb0141c0e19b575d9
b3nj4hb <benjalive12@gmail.com>
2024-08-29 20:21:30 -0500

    feat: Integrate database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/database.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
---
+++ src/config/database.config.ts
@@ -0,0 +1,17 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); // Carga las variables de entorno del archivo .env
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+	type: 'postgres',
+	host: process.env.DB_HOST,
+	port: parseInt(process.env.DB_PORT, 10),
+	username: process.env.DB_USERNAME,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	entities: [__dirname + '/../modules/**/entities/*.entity{.ts,.js}'],
+	// dropSchema: true,
+	// synchronize: true,
+	ssl: { rejectUnauthorized: true, ca: process.env.DB_CA },
+};
