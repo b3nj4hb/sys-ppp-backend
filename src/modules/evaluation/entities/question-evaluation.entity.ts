1218c99a9c14bd15e34f0247337d363821dcad80
b3nj4hb <benjalive12@gmail.com>
2024-08-31 19:01:01 -0500

    Fix: Properly join question evaluation with dimension and question structure

--- src/modules/evaluation/entities/question-evaluation.entity.ts
+++ src/modules/evaluation/entities/question-evaluation.entity.ts
@@ -2,7 +2,7 @@ import { BaseEntity } from 'src/config/base.entity';
 import { QuestionEvaluation } from '../interfaces/question-evaluation.interface';
 import { QuestionStructureEntity } from 'src/modules/evaluation-structure/entities/question-structure.entity';
 import { DimensionEvaluationEntity } from './dimension-evaluation.entity';
-import { Column, Entity, ManyToOne } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 
 @Entity({ name: 'question_evaluation' })
 export class QuestionEvaluationEntity extends BaseEntity implements QuestionEvaluation {
@@ -10,7 +10,9 @@ export class QuestionEvaluationEntity extends BaseEntity implements QuestionEval
 	score: number;
 
 	@ManyToOne(() => DimensionEvaluationEntity, (dimensionEvaluation) => dimensionEvaluation.questionEvaluation)
+	@JoinColumn({ name: 'dimension_evaluation_id' })
 	dimensionEvaluation: DimensionEvaluationEntity;
 	@ManyToOne(() => QuestionStructureEntity, (questionStructure) => questionStructure.questionEvaluation)
+	@JoinColumn({ name: 'question_structure_id' })
 	questionStructure: QuestionStructureEntity;
 }
