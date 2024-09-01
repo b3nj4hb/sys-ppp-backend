0eb28fe8aec5f6b88a64d2d016feb0300951acac
b3nj4hb <benjalive12@gmail.com>
2024-08-31 19:16:31 -0500

    Fix: Ensure foreign key relationships are correctly defined

--- src/modules/evaluation/entities/dimension-evaluation.entity.ts
+++ src/modules/evaluation/entities/dimension-evaluation.entity.ts
@@ -2,14 +2,16 @@ import { BaseEntity } from 'src/config/base.entity';
 import { DimensionEvaluation } from '../interfaces/dimension-evaluation.interface';
 import { DimensionStructureEntity } from 'src/modules/evaluation-structure/entities/dimension-structure.entity';
 import { EvaluationEntity } from './evaluation.entity';
-import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
 import { QuestionEvaluationEntity } from './question-evaluation.entity';
 
 @Entity({ name: 'dimension_evaluation' })
 export class DimensionEvaluationEntity extends BaseEntity implements DimensionEvaluation {
 	@ManyToOne(() => EvaluationEntity, (evaluation) => evaluation.dimensionEvaluation)
+	@JoinColumn({ name: 'evaluation_id' })
 	evaluation: EvaluationEntity;
 	@ManyToOne(() => DimensionStructureEntity, (dimensionStructure) => dimensionStructure.dimensionEvaluation)
+	@JoinColumn({ name: 'dimension_structure_id' })
 	dimensionStructure: DimensionStructureEntity;
 	@Column({ type: 'tinyint', nullable: true })
 	dimensionScore: number | null;
