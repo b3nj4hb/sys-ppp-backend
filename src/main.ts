d70d5eb59bc37f049402b733d60ddb1f1fa3f5d5
b3nj4hb <benjalive12@gmail.com>
2024-09-02 17:37:02 -0500

    feat: Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CORS } from './constants';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	app.enableCors(CORS);
+	await app.listen(3000);
 }
 bootstrap();
