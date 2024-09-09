95c15d750170da9e5d54490a063e723ac8889a5b
b3nj4hb <benjalive12@gmail.com>
2024-09-09 16:33:09 -0500

    feat: enable global DTO validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CORS } from './constants';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	// enable cors
 	app.enableCors(CORS);
+
+	// dto validations
+	app.useGlobalPipes(new ValidationPipe());
+
+	// port config
 	await app.listen(3000);
 }
 bootstrap();
