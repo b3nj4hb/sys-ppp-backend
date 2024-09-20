e18865cb996a96f984ffa50d1950457cb509bc5a
b3nj4hb <benjalive12@gmail.com>
2024-09-20 12:45:56 -0500

    refactor: Comment out dropSchema and synchronize options in database.config.ts

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -11,7 +11,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
 	entities: [__dirname + '/../modules/**/entities/*.entity{.ts,.js}'],
-	dropSchema: true,
-	synchronize: true,
-	ssl: { rejectUnauthorized: true, ca: process.env.DB_CA },
+	// dropSchema: true,
+	// synchronize: true,
+	ssl: { rejectUnauthorized: true },
 };
