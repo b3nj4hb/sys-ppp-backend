d7d2bedf5a573cd1cb472b5c305f3ba791725848
JOSKA <diazmoranjosue@gmail.com>
2024-10-03 13:45:34 -0500

    refactor: Eliminar archivos de datos de ciclos académicos y estudiantes y actualizar configuración de la base de datos

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,15 +3,20 @@ import * as dotenv from 'dotenv';
 
 dotenv.config(); // Carga las variables de entorno del archivo .env
 
+const isDropSchema = process.env.DB_DROP === 'true';
+const isSynchronize = process.env.DB_SYNC === 'true';
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const typeOrmConfig: TypeOrmModuleOptions = {
-	type: 'postgres',
-	host: process.env.DB_HOST,
+	type: process.env.DB_TYPE as any,
+	host: isProduction ? process.env.DB_HOST_CLOUD : process.env.DB_HOST_LOCAL,
 	port: parseInt(process.env.DB_PORT, 10),
 	username: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
 	entities: [__dirname + '/../modules/**/entities/*.entity{.ts,.js}'],
-	dropSchema: true,
-	synchronize: true,
-	// ssl: { rejectUnauthorized: true },
+	dropSchema: isDropSchema,
+	synchronize: isSynchronize,
+	logging: 'all',
+	ssl: isProduction ? { rejectUnauthorized: true, ca: process.env.DB_CA } : undefined,
 };
