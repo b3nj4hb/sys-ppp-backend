52e8f041c0b478be8d8dcb7263c3fbcb27f85d0c
b3nj4hb <benjalive12@gmail.com>
2024-10-10 16:23:43 -0500

    refactor: Update database host configuration based on environment

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -5,9 +5,11 @@ if (!process.env.DB_HOST || !process.env.DB_PORT || !process.env.DB_USERNAME || 
 	throw new Error('Faltan una o más variables de entorno necesarias');
 }
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const AppDataSource = new DataSource({
 	type: 'postgres',
-	host: process.env.DB_HOST,
+	host: isProduction ? process.env.DB_HOST_CLOUD : process.env.DB_HOST_LOCAL,
 	port: parseInt(process.env.DB_PORT, 10),
 	username: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
